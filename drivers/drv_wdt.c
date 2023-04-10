51bf4f04fb3a86b8dad44cf5e9ab3297c5f0ec03
samuelyhsu <samuel.hsu@qq.com>
2023-04-10 10:13:42 +0800

    - fix func return type

--- drivers/drv_wdt.c
+++ drivers/drv_wdt.c
@@ -70,12 +70,13 @@ int rt_wdt_init(void) {
 }
 INIT_BOARD_EXPORT(rt_wdt_init);
 
-static void wdt_sample(int argc, char *argv[]) {
+static int wdt_sample(int argc, char *argv[]) {
   rt_uint32_t timeout = 2; /* 溢出时间，单位：秒*/
   rt_device_t wdt = rt_device_find("wdt");
   rt_device_init(wdt);
   rt_device_control(wdt, RT_DEVICE_CTRL_WDT_SET_TIMEOUT, &timeout);
   rt_device_control(wdt, RT_DEVICE_CTRL_WDT_START, RT_NULL);
+  return 0;
 }
 MSH_CMD_EXPORT(wdt_sample, wdt sample);
 
